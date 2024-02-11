54e7e42f6302e7f7978a91377700655b2a60c268
Sean Herman <seanherman@gmail.com>
2024-02-10 23:32:14 -0500

    Connects to WiFi and blinks a light (#3)
    Blinks the light _in a task_

--- Makefile
+++ Makefile
@@ -1,16 +1,10 @@
 ARDUINO_CLI = arduino-cli
-BOARD = esp32:esp32:featheresp32
 
 SKETCH_DIR = Speedometer
-CONFIG_FILE = $(SKETCH_DIR)/sketch.yaml
 SKETCH = $(SKETCH_DIR)/Speedometer.ino
+BAUDRATE = 921600
 BUILD_DIR = build
 TARGET = $(BUILD_DIR)/$(notdir $(SKETCH)).bin
-PROFILE = feather
-
-monitor:
-	@cd $(SKETCH_DIR)
-	$(ARDUINO_CLI) monitor --port /dev/ttyUSB0
 
 compile: clean
 	@cd $(SKETCH_DIR)
@@ -23,4 +17,8 @@ upload: compile
 clean:
 	rm -rf $(BUILD_DIR)
 
-.PHONY: compile upload clean
+monitor:
+	@cd $(SKETCH_DIR)
+	$(ARDUINO_CLI) monitor --port /dev/ttyUSB0 --config baudrate=$(BAUDRATE)
+
+.PHONY: compile upload clean monitor
--- Speedometer/Speedometer.ino
+++ Speedometer/Speedometer.ino
@@ -1,11 +1,85 @@
+#include <WiFi.h>
+
+#include "Secrets.h"
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(921600);
+  while (!Serial) { delay(100); }
+  delay(1000);
+  Serial.println("[setup] Serial connection ready");
+
+  Serial.printf("[setup] Connecting to WiFi SSID '%s'\n", WIFI_SSID);
+  WiFi.disconnect(true);
+  WiFi.onEvent(handleWiFiEvent);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) { delay(100); }
+
+  uint32_t blink_delay = 1000;
+  xTaskCreate(TaskStrobe, "Task Strobe", 2048, (void*) &blink_delay, 2, NULL);
 }
 
-void loop() {
-  Serial.println("Starting");
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+void loop() {}
+
+void TaskStrobe(void *pvParameters) {
+  uint32_t blink_delay = *((uint32_t*)pvParameters);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  for(;;) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(blink_delay);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(blink_delay);
+  }
+}
+
+char* formatWiFiEvent(WiFiEvent_t event) {
+  switch (event) {
+    case ARDUINO_EVENT_WIFI_READY: return "READY";
+    case ARDUINO_EVENT_WIFI_SCAN_DONE: return "SCAN_DONE";
+    case ARDUINO_EVENT_WIFI_STA_START: return "STA_START";
+    case ARDUINO_EVENT_WIFI_STA_STOP: return "STA_STOP";
+    case ARDUINO_EVENT_WIFI_STA_CONNECTED: return "STA_CONNECTED";
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED: return "STA_DISCONNECTED";
+    case ARDUINO_EVENT_WIFI_STA_AUTHMODE_CHANGE: return "STA_AUTHMODE_CHANGE";
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP: return "STA_GOT_IP";
+    case ARDUINO_EVENT_WIFI_STA_LOST_IP: return "STA_LOST_IP";
+    default: return "n/a";
+  }
+}
+
+void handleWiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %s (%d)\n", formatWiFiEvent(event), event);
+
+  switch (event) {
+    case ARDUINO_EVENT_WIFI_READY: 
+      Serial.println("[WiFi-event] WiFi interface ready");
+      break;
+    case ARDUINO_EVENT_WIFI_SCAN_DONE:
+      Serial.println("[WiFi-event] Completed scan for access points");
+      break;
+    case ARDUINO_EVENT_WIFI_STA_START:
+      Serial.println("[WiFi-event] WiFi client started");
+      break;
+    case ARDUINO_EVENT_WIFI_STA_STOP:
+      Serial.println("[WiFi-event] WiFi clients stopped");
+      break;
+    case ARDUINO_EVENT_WIFI_STA_CONNECTED:
+      Serial.println("[WiFi-event] Connected to access point");
+      break;
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+      Serial.println("[WiFi-event] Disconnected from WiFi access point");
+      break;
+    case ARDUINO_EVENT_WIFI_STA_AUTHMODE_CHANGE:
+      Serial.println("[WiFi-event] Authentication mode of access point has changed");
+      break;
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+      Serial.print("[WiFi-event] Obtained IP address: ");
+      Serial.println(WiFi.localIP());
+      break;
+    case ARDUINO_EVENT_WIFI_STA_LOST_IP:
+      Serial.println("[WiFi-event] Lost IP address and IP address is reset to 0");
+      break;
+    default: break;
+  }
 }
