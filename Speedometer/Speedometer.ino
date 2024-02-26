3691d5c691f9b69427b092f4ae3588a71b36dc63
Sean Herman <seanherman@gmail.com>
2024-02-25 23:52:15 -0500

    works but noisy as hell

--- Speedometer/Speedometer.ino
+++ Speedometer/Speedometer.ino
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -12,11 +13,23 @@
 #include "Config.h"
 #include "Secrets.h"
 
+QueueHandle_t speedUpdateQueue;
+
+// Speed update (velocity and acceleration)
+typedef struct SpeedUpdate {
+  unsigned long timestamp;
+  int velocity;
+  float accelerationX;
+  float accelerationY;
+  float accelerationZ;
+} SpeedUpdate_t;
+
 // Assign the current time in ISO8601 format to buff and return 0,
 // or a -1 to represent an error for cases when buff doesn't fit
 // the time string or the time cannot be generated for some other
 // reason.
 int currentTimeISO8601(char* buff, size_t buffSize) {
+  // TODO(sean) rewrite w/ baseline + ticks
   if (!buff || buffSize < 21) {
     Serial.println("[currentTimeISO8601] Invalid buffer");
     return -1;
@@ -32,19 +45,10 @@ int currentTimeISO8601(char* buff, size_t buffSize) {
     return -1;
   }
 
-  Serial.println("[currentTimeISO8601] Formatting current time");
   strftime(buff, buffSize, "%Y-%m-%dT%H:%M:%SZ", utcTime);
+  Serial.printf("[currentTimeISO8601] Formatted current time: %s\n", buff);
   return 0;
 }
-
-// Speed update (velocity and acceleration)
-typedef struct SpeedUpdate {
-  char timestamp[21];
-  int velocity;
-  int accelerationX;
-  int accelerationY;
-  int accelerationZ;
-} SpeedUpdate_t;
 
 // Assign the payload JSON to the buffer, returning the number of
 // characters written to the buffer, or a number < 0 when the payload
@@ -93,82 +97,161 @@ void setClock() {
   Serial.print(asctime(&timeinfo));
 }
 
+// Return the magnitude of the vector
+float magnitude(float x, float y, float z) {
+  return sqrt(x*x + y*y + z*z);
+}
+
 // Record the current velocity from the accelerometer.
-void TaskCaptureSpeed(void *pvParameters) {
+void TaskMeasureSpeed(void *pvParameters) {
   Adafruit_LSM6DSOX sensor;
   sensor.setAccelDataRate(CONFIG_ACCEL_DATA_RATE);
   sensor.setAccelRange(CONFIG_ACCEL_RANGE);
   
   if (!sensor.begin_I2C()) {
-    Serial.println("Failed to find LSM6DS chip");
+    Serial.println("[TaskMeasureSpeed] Failed to find LSM6DS chip");
     return;
   }
-  Serial.println("LSM6DS found.");
+  Serial.println("[TaskMeasureSpeed] LSM6DS found.");
+
+  float accelX, accelY, accelZ; // Accelerometer readings
+  float gyroX, gyroY, gyroZ; // Gyroscope readings
+  float roll, pitch; // Calculated roll and pitch angles
+  float accAngleX, accAngleY; // Accelerometer-derived angle estimates
+  float gyroAngleX, gyroAngleY; // Gyroscope-derived angle estimates
+  float finalAngleX, finalAngleY; // Fused angle estimates
+  float linearAccelX, linearAccelY, linearAccelZ; // Linear acceleration components
+  float velocityX = 0.0, velocityY = 0.0, velocityZ = 0.0; // Velocity components
+  const float alpha = 0.98;  // Weighting factor for accelerometer data
+  const float accelGravity = 9.81;
+
+  unsigned long prevTimestamp = 0;
 
   for (;;) {
-    sensors_event_t accel, gyro, mag, temp;
-    sensor.getEvent(&accel, &gyro, &temp);
+    /*
+    char currentTime[21];
+    if (!currentTimeISO8601(currentTime, sizeof(currentTime)) < 0) {
+      Serial.println("[TaskMeasureSpeed] Failed to create timestamp");
+      continue;
+    }
+    */
+
+    unsigned long currentTimestamp = millis();
+    float elapsedMs = (currentTimestamp - prevTimestamp) / 1000.0;
+
+    Serial.printf("[TaskMeasureSpeed] Capturing sensor readings at: %d\n", currentTimestamp);
+    sensors_event_t measure, gyro, temp;
+    sensor.getEvent(&measure, &gyro, &temp);
     
     /* Display the results (acceleration is measured in m/s^2) */
-    Serial.print("\t\tAccel X: ");
-    Serial.print(accel.acceleration.x, 4);
+    Serial.print("[TaskMeasureSpeed] Accel X: ");
+    Serial.print(measure.acceleration.x, 4);
     Serial.print(" \tY: ");
-    Serial.print(accel.acceleration.y, 4);
+    Serial.print(measure.acceleration.y, 4);
     Serial.print(" \tZ: ");
-    Serial.print(accel.acceleration.z, 4);
+    Serial.print(measure.acceleration.z, 4);
     Serial.println(" \tm/s^2 ");
 
     /* Display the results (rotation is measured in rad/s) */
-    Serial.print("\t\tGyro  X: ");
-    Serial.print(gyro.gyro.x, 4);
+    Serial.print("[TaskMeasureSpeed] Gyro X: ");
+    Serial.print(gyro.gyro.x);
     Serial.print(" \tY: ");
-    Serial.print(gyro.gyro.y, 4);
+    Serial.print(gyro.gyro.y);
     Serial.print(" \tZ: ");
-    Serial.print(gyro.gyro.z, 4);
-    Serial.println(" \tradians/s ");
-
-    Serial.print("\t\tTemp   :\t\t\t\t\t");
-    Serial.print(temp.temperature);
-    Serial.println(" \tdeg C");
+    Serial.print(gyro.gyro.z);
+    Serial.println(" radians/s ");
     Serial.println();
 
-    delay(CONFIG_SENSOR_UPDATE_MILLISECONDS);
+    accelX = measure.acceleration.x;
+    accelY = measure.acceleration.y;
+    accelZ = measure.acceleration.z;
+    gyroX = measure.gyro.x;
+    gyroY = measure.gyro.y;
+    gyroZ = measure.gyro.z;
+
+    // Calculate roll and pitch angles using accelerometer data
+    roll = atan2(accelY, accelZ) * 180.0 / PI;
+    pitch = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ)) * 180.0 / PI;
+
+    // Calculate accelerometer-derived angle estimates
+    accAngleX = roll;
+    accAngleY = pitch;
+    // Integrate gyroscope data to obtain gyroAngleX and gyroAngleY
+    gyroAngleX += gyroX * elapsedMs;
+    gyroAngleY += gyroY * elapsedMs;
+
+    // Combine accelerometer and gyroscope data using complementary filter
+    finalAngleX = alpha * gyroAngleX + (1 - alpha) * accAngleX;
+    finalAngleY = alpha * gyroAngleY + (1 - alpha) * accAngleY;
+
+    // Calculate linear acceleration components by subtracting gravitational component
+    linearAccelX = accelX * cos(finalAngleY * PI / 180.0) + accelZ * sin(finalAngleY * PI / 180.0);
+    linearAccelY = accelY * cos(finalAngleX * PI / 180.0) - accelZ * sin(finalAngleX * PI / 180.0);
+    linearAccelZ = accelZ - 9.81; // Subtract gravity (approx. 9.81 m/s^2)
+
+    // Integrate linear acceleration to obtain velocity
+    velocityX += linearAccelX * elapsedMs;
+    velocityY += linearAccelY * elapsedMs;
+    velocityZ += linearAccelZ * elapsedMs;
+
+    // Calculate speed (magnitude of velocity)
+    Serial.print("[TaskMeasureSpeed] Veloc X: ");
+    Serial.print(velocityX, 4);
+    Serial.print(" \tY: ");
+    Serial.print(velocityY, 4);
+    Serial.print(" \tZ: ");
+    Serial.print(velocityZ, 4);
+    Serial.println(" \tm/s ");
+
+    float speed = magnitude(velocityX, velocityY, velocityZ);
+    Serial.printf("[TaskMeasureSpeed] Speed: %d\n", speed);
+
+    SpeedUpdate_t update;
+    update.timestamp = currentTimestamp;
+    //strcpy(update.timestamp, "2024-01-01T00:00:00Z");
+    update.velocity = speed;
+    update.accelerationX = linearAccelX;
+    update.accelerationY = linearAccelY;
+    update.accelerationZ = linearAccelZ;
+
+    // Move on if there is no room in the queue
+    if(xQueueSendToBack(speedUpdateQueue, &update, 0) != pdPASS) {
+      Serial.println("[TaskMeasureSpeed] Failed to send update");
+      continue;
+    }
+    Serial.println("[TaskMeasureSpeed] Sent speed update");
+    vTaskDelay(pdMS_TO_TICKS(CONFIG_SENSOR_UPDATE_MILLISECONDS));
   }
 }
 
+
 // Forward the speed update to the remote server.
-void TaskRecordSpeedUpdate(void *pvParameters) {
-  Serial.println("[TaskRecordSpeedUpdate] Starting...");
+#ifdef CONFIG_ENABLE_SPEED_RECORDING
+void TaskRecordSpeed(void *pvParameters) {
+  Serial.println("[TaskRecordSpeed] Starting...");
 
   for(;;) {
     WiFiClientSecure *client = new WiFiClientSecure;
     if (!client) {
-      Serial.println("[TaskRecordSpeedUpdate] Failed to create WiFiClientSecure.");
-      delay(5000);
+      Serial.println("[TaskRecordSpeed] Failed to create WiFiClientSecure.");
+      vTaskDelay(pdMS_TO_TICKS(5000));
       continue;
     }
-    Serial.printf("Setting CA certificate:\n%s\n", CONFIG_ROOT_CA_CERTIFICATE);
     client->setInsecure();
     // TODO(sean) fix TLS with local server + self-signed certificate
     //client->setCACert(CONFIG_ROOT_CA_CERTIFICATE);
 
     {
       HTTPClient http;
-      Serial.printf("[TaskRecordSpeedUpdate] begin request to %s\n", CONFIG_API_URL);
-      delay(100);
+      Serial.printf("[TaskRecordSpeed] begin request to %s\n", CONFIG_API_URL);
+      vTaskDelay(pdMS_TO_TICKS(100));
 
       if (http.begin(*client, String(CONFIG_API_URL))) {
-        Serial.println("[TaskRecordSpeedUpdate] Requesting...");
-
-        // TODO(sean) move timestamp creation to code handling accelerometer updates
-        char currentTime[21];
-        if (!currentTimeISO8601(currentTime, sizeof(currentTime)) < 0) {
-          Serial.println("[TaskRecordSpeedUpdate] Failed to create timestamp");
-          continue;
-        }
+        Serial.println("[TaskRecordSpeed] Requesting...");
 
         struct SpeedUpdate update;
-        strcpy(update.timestamp, currentTime);
+        char currentTimestamp[] = "2024-01-01T00:00:00.000Z";
+        strcpy(update.timestamp, currentTimestamp);
         update.velocity = 0;
         update.accelerationX = 0;
         update.accelerationY = 0;
@@ -177,10 +260,10 @@ void TaskRecordSpeedUpdate(void *pvParameters) {
         http.addHeader("Content-Type", "application/json");
         char payload[150];
         if (serializeSpeedUpdate(payload, sizeof(payload), &update) < 0) {
-          Serial.println("[TaskRecordSpeedUpdate] Failed to serialize payload");
+          Serial.println("[TaskRecordSpeed] Failed to serialize payload");
           continue;
         }
-        Serial.printf("[TaskRecordSpeedUpdate] sending payload: %s", payload);
+        Serial.printf("[TaskRecordSpeed] sending payload: %s\n", payload);
 
         int httpCode = http.POST(String(payload));
         if (httpCode > 0) {
@@ -189,27 +272,29 @@ void TaskRecordSpeedUpdate(void *pvParameters) {
             case HTTP_CODE_OK:
             case HTTP_CODE_ACCEPTED:
             case HTTP_CODE_MOVED_PERMANENTLY:
-            Serial.printf("[TaskRecordSpeedUpdate] success response %d: %s\n", httpCode, response.c_str());
+            Serial.printf("[TaskRecordSpeed] success response %d: %s\n", httpCode, response.c_str());
             break;
             default:
-            Serial.printf("[TaskRecordSpeedUpdate] unexpected response %d: %s\n", httpCode, response.c_str());
+            Serial.printf("[TaskRecordSpeed] unexpected response %d: %s\n", httpCode, response.c_str());
             break;
           }
         } else {
-          Serial.printf("[TaskRecordSpeedUpdate] Request failed, error: %s\n", http.errorToString(httpCode).c_str());
+          Serial.printf("[TaskRecordSpeed] Request failed, error: %s\n", http.errorToString(httpCode).c_str());
         }
 
         http.end();
       } else {
-        Serial.println("[TaskRecordSpeedUpdate] Unable to connect");
+        Serial.println("[TaskRecordSpeed] Unable to connect");
       }
     }
 
     delete client;
-    Serial.println("[TaskRecordSpeedUpdate] Waiting 5s...");
-    delay(5000);
+    Serial.println("[TaskRecordSpeed] Waiting 5s...");
+    // TODO(sean) remove when we're processing queue messages
+    vTaskDelay(pdMS_TO_TICKS(5000));
   }
 }
+#endif
 
 // Output the velocity to the 7-Segment display.
 void TaskUpdateDisplay(void *pvParameters) {
@@ -219,22 +304,19 @@ void TaskUpdateDisplay(void *pvParameters) {
   matrix.begin(0x70);
   matrix.clear();
 
-  uint16_t value = 0;
-  boolean drawDots = false;
-  for(uint16_t counter = 0; counter < 9999; counter++) {
-    Serial.printf("[TaskUpdateDisplay] Displaying %d\n", counter);
-    matrix.writeDigitNum(0, (counter / 1000), drawDots);
-    matrix.writeDigitNum(1, (counter / 100) % 10, drawDots);
-    matrix.drawColon(drawDots);
-    matrix.writeDigitNum(3, (counter / 10) % 10, drawDots);
-    matrix.writeDigitNum(4, counter % 10, drawDots);
-    matrix.writeDisplay();
-    delay(1000);
-  }
+  SpeedUpdate_t update;
 
-  matrix.clear();
-  matrix.println(0);
-  matrix.writeDisplay();
+  for(;;) {
+    if (xQueueReceive(speedUpdateQueue, &update, portMAX_DELAY) != pdPASS) {
+      Serial.println("[TaskUpdateDisplay] Failed to receive message from queue");
+    }
+    Serial.println("[TaskUpdateDisplay] Received message from queue");
+    matrix.writeDigitNum(0, (update.velocity / 1000));
+    matrix.writeDigitNum(1, (update.velocity / 100) % 10);
+    matrix.writeDigitNum(3, (update.velocity / 10) % 10);
+    matrix.writeDigitNum(4, update.velocity % 10);
+    matrix.writeDisplay();
+  }
 }
 
 char* formatWiFiEvent(WiFiEvent_t event) {
@@ -307,9 +389,17 @@ void setup() {
   
   setClock();
 
-  xTaskCreate(TaskUpdateDisplay, "TaskUpdateDisplay", 2049, NULL, 2, NULL);
-  xTaskCreate(TaskRecordSpeedUpdate, "TaskRecordSpeedUpdate", 16384, NULL, 2, NULL);
-  xTaskCreate(TaskCaptureSpeed, "TaskCaptureSpeed", 2049, NULL, 2, NULL);
+  speedUpdateQueue = xQueueCreate(10, sizeof(SpeedUpdate_t));
+  if (speedUpdateQueue == NULL) {
+    Serial.println("[setup] Failed to create speed update queue");
+    return;
+  }
+
+  xTaskCreate(TaskUpdateDisplay, "TaskUpdateDisplay", 4096, NULL, 2, NULL);
+  xTaskCreate(TaskMeasureSpeed, "TaskMeasureSpeed", 16384, NULL, 3, NULL);
+  #ifdef CONFIG_ENABLE_SPEED_RECORDING
+  xTaskCreate(TaskRecordSpeed, "TaskRecordSpeed", 16384, NULL, 1, NULL);
+  #endif
 }
 
 void loop() {}
